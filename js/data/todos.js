bcf25972f99a719521f708d9e059235352ca80ea
gintaregud <63239813+gintaregud@users.noreply.github.com>
2020-05-13 14:37:10 +0300

    .

---
+++ js/data/todos.js
@@ -0,0 +1,32 @@
+let todo_id = 4;
+
+let todo_list = [
+    {
+        id: 1,
+        description: 'Pasigaminti kavos',
+        created_on: '2020-05-05 15:01',
+        deadline: '2020-05-06 12:55',
+        status: 'todo'
+    },
+    {
+        id: 2,
+        description: 'Pravesti paskaita',
+        created_on: '2020-05-05 15:02',
+        deadline: '2020-05-06 13:00',
+        status: 'in-progress'
+    },
+    {
+        id: 3,
+        description: 'Pradeti generuoti paskaitos irasus',
+        created_on: '2020-05-05 15:03',
+        deadline: '2020-05-06 17:00',
+        status: 'done'
+    },
+    {
+        id: 4,
+        description: 'Sugalvoti nauja uzduoti',
+        created_on: '2020-05-07 14:28',
+        deadline: '2020-05-11 13:17',
+        status: 'in-progress'
+    }
+];
